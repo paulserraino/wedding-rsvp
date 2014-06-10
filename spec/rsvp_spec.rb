8be4d530765202b80e6f753f1148c62f8f3c48d5
paul <paul@sfcg.com>
2014-06-10 16:20:42 -0500

    writing more rsvp tests

--- spec/rsvp_spec.rb
+++ spec/rsvp_spec.rb
@@ -42,8 +42,8 @@ describe RSVP do
 	      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
 	                     foo@bar_baz.com foo@bar+baz.com]
 	      addresses.each do |invalid_address|
-	        @user.email = invalid_address
-	        expect(@user).not_to be_valid
+	        @rsvp.email = invalid_address
+	        expect(@rsvp).not_to be_valid
 	      end
 	    end
 	  end
@@ -52,8 +52,8 @@ describe RSVP do
 	    it "should be valid" do
 	      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
 	      addresses.each do |valid_address|
-	        @user.email = valid_address
-	        expect(@user).to be_valid
+	        @rsvp.email = valid_address
+	        expect(@rsvp).to be_valid
 	      end
 	    end
 	  end
