0a254f9369274ba3e729b863ded8292fe5389224
paul <pserraino99@gmail.com>
2014-06-18 12:28:47 -0500

    adding rsvp widget

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,47 @@
 (function ($) {
-	var colors = ["#F22613", "#19B5FE", "deeppink", "fuchsia", "lightgreen", 
-	"lime", "yellow", "springgreen", "deepskyblue", "orangered"];
 
-	var images = ["/images/sample1.jpg", "/images/sample2.jpg"];
+	var RSVPUI = {
+		colors: ["#F22613", "#19B5FE", "deeppink", "fuchsia", "lightgreen", "lime", 
+		"yellow", "springgreen", "deepskyblue", "orangered"],
+		images: ["/images/sample1.jpg", "/images/sample2.jpg"],
+		init: function () {
+			// random heart color when page loads
+			this.setHeartColor(this.randomColor());
+		},
+		submit: function () {
+			$("#rsvp-form").on("submit", function () {
+				e.preventDefault();
+
+				$.ajax({
+					type: "POST",
+					url: "/rsvp/new",
+					data: $(this).serialize()
+				}).done(function (data) {
+
+				}).fail(function (err) {
+					console.log("Failed to rsvp ", err);
+				});
+
+			});
+		},
+		randomColor: function () {
+			var rc = Math.floor(Math.random() * ((colors.length-1) + 1));
+			return this.colors[rc];
+		}
+		setHeartColor: function (color) {
+			$("span.heart").css("color", color);
+		},
+		imageSlider: function () {
+			$("span.heart").on("click", function (e) {
+				e.preventDefault();
+			});
+		}
+
+	}
 
 	$(document).ready(function () {
-		var rc = Math.floor(Math.random() * ((colors.length-1) + 1));
-		var $heart = $("span.heart");
-		var $rsvpForm = $("#rsvp-form");
-		var $mainSection = $("#main-section");
-
-		$heart.css("color", colors[rc]);
-
-		$heart.on("click", function (e) {
-			e.preventDefault();
-
-		});
-
-		$rsvpForm.on("submit", function (e) {
-			e.preventDefault();
-
-			$.ajax({
-				type: "POST",
-				url: "/rsvp/new",
-				data: $(this).serialize()
-			}).done(function (data) {
-
-			}).fail(function (err) {
-				console.log("Failed to rsvp ", err);
-			});
-		});
-
-		/*
-		setInterval(function () {
-			var rand = Math.floor(Math.random() * ((images.length-1) + 1));
-
-			$mainSection.css("background-image", "url("+images[rand]+")");
-		}, 1000);
-		*/
-
-
-
+		RSVPUI.init();
 	});
-
-	
 
 
 })(jQuery);
