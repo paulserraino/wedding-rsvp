1b42cb28c7a711b9105e66c2d14c9c944f682e04
Paul Serraino <pserraino99@gmail.com>
2015-05-16 19:07:20 -0500

    formatting

--- app/views/rsvp-view.js
+++ app/views/rsvp-view.js
@@ -3,64 +3,64 @@ var $ = require('jquery');
 Backbone.$ = $;
 
 module.exports = Backbone.View.extend({
-	events: {
-		'submit': 'submit'
-	}
+  events: {
+    'submit': 'submit'
+  }
 
 , fieldMap: {
-		email: '#email'
-	, first_name: '#first_name'
-	, last_name: '#last_name'
-	, reservation: '#reservation:checked'
-	, plus_one: '#plus_one'
-	, message: '#message'
-	}
+    email: '#email'
+  , first_name: '#first_name'
+  , last_name: '#last_name'
+  , reservation: '#reservation:checked'
+  , plus_one: '#plus_one'
+  , message: '#message'
+  }
 , initialize: function () {
-	console.log('init')
+  console.log('init')
 }
 , submit: function (e) {
-		e.preventDefault();
-		this.clearErrors();
+    e.preventDefault();
+    this.clearErrors();
 
-		this.model.set({
-			email: this.$el.find(this.fieldMap.email).val()
-		, first_name: this.$el.find(this.fieldMap.first_name).val()
-		, last_name: this.$el.find(this.fieldMap.last_name).val()
-		, reservation: this.$el.find(this.fieldMap.reservation).val()
-		, plus_one: this.$el.find(this.fieldMap.plus_one).val()
-		, message: this.$el.find(this.fieldMap.message).val()
-		})
+    this.model.set({
+      email: this.$el.find(this.fieldMap.email).val()
+    , first_name: this.$el.find(this.fieldMap.first_name).val()
+    , last_name: this.$el.find(this.fieldMap.last_name).val()
+    , reservation: this.$el.find(this.fieldMap.reservation).val()
+    , plus_one: this.$el.find(this.fieldMap.plus_one).val()
+    , message: this.$el.find(this.fieldMap.message).val()
+    })
 
-		if (!this.model.isValid()) {
-			return this.displayError(this.model.validationError);
-		}
+    if (!this.model.isValid()) {
+      return this.displayError(this.model.validationError);
+    }
 
-		this.model.save(null, {
-			success: function () {
-				console.log('saved!')
-			}
-		, errors: function () {
-				console.log('failed')
-			}
-		});
-	}
+    this.model.save(null, {
+      success: function () {
+        console.log('saved!')
+      }
+    , errors: function () {
+        console.log('failed')
+      }
+    });
+  }
 
 , displayError: function (error, selector) {
-		selector = selector || '[name=":p"]'.replace(':p', error.prop);
-		var $el = this.$el.find(selector);
-		var $span = $('<span class="errors">').text(error.message);
-		var red = '#D91E18';
+    selector = selector || '[name=":p"]'.replace(':p', error.prop);
+    var $el = this.$el.find(selector);
+    var $span = $('<span class="errors">').text(error.message);
+    var red = '#D91E18';
 
-		$el.addClass('errors');
-		$el.siblings('label').append($span);
-		$el.focus();
+    $el.addClass('errors');
+    $el.siblings('label').append($span);
+    $el.focus();
 
-		return this;
-	}
+    return this;
+  }
 
 , clearErrors: function() {
-		$('label > span.errors').remove();
-		$('.errors').removeClass('errors');
-		return this;
-	}
+    $('label > span.errors').remove();
+    $('.errors').removeClass('errors');
+    return this;
+  }
 });
